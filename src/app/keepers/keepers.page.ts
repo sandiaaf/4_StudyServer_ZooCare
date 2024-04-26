97456bff51850030f7c5f6e4f585000e4c659fa1
AhmadRamadhani129 <ahmad.ramadhani.aribowo@gmail.com>
2024-04-27 01:46:11 +0700

    update keeper

--- src/app/keepers/keepers.page.ts
+++ src/app/keepers/keepers.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-keepers',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KeepersPage implements OnInit {
 
-  constructor() { }
+
+  keepers:any[] = []
+
+  constructor(private router:Router) { }
 
   ngOnInit() {
+    
   }
 
 }
