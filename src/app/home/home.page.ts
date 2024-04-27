cca8428e9583624146b3acaef0e28dc1de874f9a
RevaRayhan <revarayhansyah@gmail.com>
2024-04-27 17:39:05 +0700

    Guide pada page home. Diagnosis dan list guide beserta langkah2nya

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  guides: any[] = []
+  actions: any[] = []
+
+  nafsuMakan = false
+  tingkatEnergi = false
+  garuk = false
+  lesu = false
+  sulitNapas = false
+  agresif = false
+
+  constructor(private http: HttpClient, private alertController: AlertController) { }
 
   ngOnInit() {
+    this.guideList().subscribe(
+      (data) => {
+        this.guides = data
+      }
+    )
+    this.guide_actionList().subscribe(
+      (data) => {
+        this.actions = data
+      }
+    )
   }
 
+  guideList(): Observable<any> {
+    return this.http.get("http://localhost/zoocaredb/guides.php");
+  }
+
+  guide_actionList(): Observable<any> {
+    return this.http.get("http://localhost/zoocaredb/guide_actions.php");
+  }
+
+  checkPrognosis() {
+    let prognosis = ""
+    if (this.nafsuMakan == true && this.tingkatEnergi == true) {
+      prognosis = "Kemungkinan gangguan Pencernaan, seperti maag atau sakit perut biasa"
+    }
+    else if (this.tingkatEnergi == true && this.lesu == true && this.nafsuMakan == true) {
+      prognosis = "Kemungkinan infeksi virus atau bakteri"
+    }
+    else if (this.tingkatEnergi == true && this.lesu == true && this.nafsuMakan == false) {
+      prognosis = "kemungkinan terdapat penyakit ginjal, terutama jika ditemani dengan sering buang air kecil"
+    }
+    else if (this.tingkatEnergi == true && this.garuk == true) {
+      prognosis = "Kemungkinan reaksi alergi atau infeksi kulit"
+    }
+    else if (this.sulitNapas == true) {
+      prognosis = "Kemungkinan infeksi saluran pernapasan"
+    }
+    else if (this.garuk == true) {
+      prognosis = "Banyak kemungkinan, seperti alergi, parasit, infeksi kulit, dan sebagainya"
+    }
+    else if (this.agresif == true) {
+      prognosis = "Kemungkinan rasa sakit atau ketidaknyamanan, ketakutan, atau gelisah"
+    }
+    else if (this.nafsuMakan == true) {
+      prognosis = "Kemungkinan gangguan pencernaan"
+    }
+    else if (this.tingkatEnergi == true || this.lesu == true) {
+      prognosis = "Kemungkinan penyakit, seperti masalah saluran pencernaan, artritis, atau depresi"
+    }
+    else {
+      prognosis = "Kekurangan data untuk menarik kesimpulan"
+    }
+    this.presentAlert(prognosis)
+    this.nafsuMakan = false
+    this.tingkatEnergi = false
+    this.garuk = false
+    this.lesu = false
+    this.sulitNapas = false
+    this.agresif = false
+  }
+
+  async presentAlert(msg: string) {
+    const alert = await this.alertController.create({
+      header: 'Information',
+      message: msg,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 }
